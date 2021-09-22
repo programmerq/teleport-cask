4ad6f31e7cd07646b37090ecc1182d9527737193
Jeff Anderson <jeff@goteleport.com>
2021-09-22 14:41:06 -0400

    trying versioned cask

---
+++ Casks/teleport-ent@7.0.rb
@@ -0,0 +1,21 @@
+cask "teleport-ent" do
+  version "7.0.3"
+  sha256 "fe199036e75909575352f1186ad5d2826200736714e6e9d2a3ce1a9ecc84b4be"
+
+  url "https://get.gravitational.com/teleport-ent-#{version}.pkg",
+      verified: "get.gravitational.com"
+  name "teleport-ent"
+  desc "Teleport is a gateway for managing access to clusters of Linux servers via SSH or the Kubernetes API. Enterprise Edition"
+  homepage "https://goteleport.com/"
+  pkg "teleport-ent-#{version}.pkg"
+
+  uninstall pkgutil: [
+    "com.gravitational.teleport"
+  ]
+
+  conflicts_with formula: "teleport", cask: ["teleport", "tsh"]
+
+  caveats do
+    license "https://dashboard.gravitational.com/web/"
+  end
+end
