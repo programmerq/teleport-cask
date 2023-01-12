fdcb35e7a1c3ec7948f477d4b10d7d7cadee4747
Jeff Anderson <jeff@goteleport.com>
2023-01-12 15:57:55 -0500

    update oss too

--- update.sh
+++ update.sh
@@ -85,6 +85,40 @@ cask "teleport-ent" do
 end
 EOF
 
+cat > ./Casks/teleport.rb <<EOF
+cask "teleport" do
+  module Utils
+    def self.version
+      return "${latest}"
+    end
+    def self.getsha
+      require 'net/http'
+      return Net::HTTP.get(URI("https://get.gravitational.com/teleport-#{version}.pkg.sha256")).split()[0]
+    end
+  end
+
+  version "#{Utils.version}"
+  sha256 "#{Utils.getsha}"
+
+  url "https://get.gravitational.com/teleport-#{version}.pkg",
+      verified: "get.gravitational.com"
+  name "teleport"
+  desc "Teleport is a gateway for managing access to clusters of Linux servers via SSH or the Kubernetes API."
+  homepage "https://goteleport.com/"
+  pkg "teleport-#{version}.pkg"
+
+  uninstall pkgutil: [
+    "com.gravitational.teleport"
+  ]
+
+  conflicts_with formula: "teleport", cask: ["teleport-ent", "tsh", "teleport-ent@7.3", "teleport-ent@8.0, teleport-ent@9.0, teleport-ent@10.0"]
+
+  caveats do
+    license "Apache-2.0"
+  end
+end
+EOF
+
 cat > ./Casks/teleport-ent@${latestamajor}.0.rb <<EOF
 cask "teleport-ent@${latestamajor}.0" do
   module Utils
