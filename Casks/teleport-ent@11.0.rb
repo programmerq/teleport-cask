c6cb8f5760ceb0f3e7dbd5894eb5ae9663abb464
Jeff Anderson <jeff@goteleport.com>
2023-02-09 10:57:48 -0500

    11.3.3

--- Casks/teleport-ent@11.0.rb
+++ Casks/teleport-ent@11.0.rb
@@ -1,7 +1,7 @@
-cask "teleport-ent@10.0" do
+cask "teleport-ent@11.0" do
   module Utils
     def self.version
-      return "10.3.12"
+      return "11.3.3"
     end
     def self.getsha
       require 'net/http'
@@ -23,7 +23,7 @@ cask "teleport-ent@10.0" do
     "com.gravitational.teleport"
   ]
 
-  conflicts_with formula: "teleport", cask: ["teleport", "tsh", "teleport-ent@7.3", "teleport-ent@8.0, teleport-ent@9.0, teleport-ent@11.0, teleport-ent"]
+  conflicts_with formula: "teleport", cask: ["teleport", "tsh", "teleport-ent@7.3", "teleport-ent@8.0, teleport-ent@9.0, teleport-ent@10.0, teleport-ent"]
 
   caveats do
     license "https://dashboard.gravitational.com/web/"
