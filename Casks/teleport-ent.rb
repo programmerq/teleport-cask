e0e2ec1ef51a023200de0bf7d3514e8b8347d585
Jeff Anderson <jeff@goteleport.com>
2023-04-28 13:41:18 -0400

    update conflicts

--- Casks/teleport-ent.rb
+++ Casks/teleport-ent.rb
@@ -23,7 +23,7 @@ cask "teleport-ent" do
     "com.gravitational.teleport"
   ]
 
-  conflicts_with formula: "teleport", cask: ["teleport", "teleport-ent@8.0, teleport-ent@9.0, teleport-ent@10.0"]
+  conflicts_with formula: "teleport", cask: ["teleport", "teleport-ent@8.0, teleport-ent@9.0"]
 
   caveats do
     license "https://dashboard.gravitational.com/web/"
