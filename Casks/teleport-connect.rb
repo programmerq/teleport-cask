d6023be5dbf21c4eae1fde138ee95015facdf98f
Jeff Anderson <jeff@goteleport.com>
2023-08-08 09:29:29 -0400

    first try for teleport connect

---
+++ Casks/teleport-connect.rb
@@ -0,0 +1,27 @@
+cask "teleport-connect" do
+  module Utils
+    def self.version
+      return "13.3.1"
+    end
+    def self.getsha
+      require 'net/http'
+      return Net::HTTP.get(URI("https://get.gravitational.com/Teleport Connect-#{version}.dmg.sha256")).split()[0]
+    end
+  end
+
+  version "#{Utils.version}"
+  sha256 "#{Utils.getsha}"
+
+  url "https://get.gravitational.com/Teleport Connect-#{version}.dmg",
+      verified: "get.gravitational.com"
+  name "Teleport Connect"
+  desc "Teleport Connect is a user-friendly GUI desktop application that provides the same access to servers, databases, and Kubernetes clusters as the Teleport command-line client (tsh)."
+  homepage "https://goteleport.com/"
+  app "Teleport Connect.app"
+
+  uninstall quit: "gravitational.teleport.connect"
+
+  caveats do
+    license "Apache-2.0"
+  end
+end
--- update.sh
+++ update.sh
@@ -102,6 +102,36 @@ cask "teleport-ent" do
 end
 EOF
 
+cat > ./Casks/teleport-connect.rb <<EOF
+cask "teleport-connect" do
+  module Utils
+    def self.version
+      return "${osslatest}"
+    end
+    def self.getsha
+      require 'net/http'
+      return Net::HTTP.get(URI("https://get.gravitational.com/Teleport Connect-#{version}.dmg.sha256")).split()[0]
+    end
+  end
+
+  version "#{Utils.version}"
+  sha256 "#{Utils.getsha}"
+
+  url "https://get.gravitational.com/Teleport Connect-#{version}.dmg",
+      verified: "get.gravitational.com"
+  name "Teleport Connect"
+  desc "Teleport Connect is a user-friendly GUI desktop application that provides the same access to servers, databases, and Kubernetes clusters as the Teleport command-line client (tsh)."
+  homepage "https://goteleport.com/"
+  app "Teleport Connect.app"
+
+  uninstall quit: "gravitational.teleport.connect"
+
+  caveats do
+    license "Apache-2.0"
+  end
+end
+EOF
+
 cat > ./Casks/teleport.rb <<EOF
 cask "teleport" do
   module Utils
