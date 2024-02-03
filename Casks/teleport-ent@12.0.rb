e5095aac5f18905b275946ff2f6d8b3a529740bc
Jeff Anderson <jeff@goteleport.com>
2024-02-03 13:08:30 -0500

    12.4.33

--- Casks/teleport-ent@12.0.rb
+++ Casks/teleport-ent@12.0.rb
@@ -23,7 +23,7 @@ cask "teleport-ent@12.0" do
     "com.gravitational.teleport"
   ]
 
-  conflicts_with formula: "teleport", cask: ["teleport", "teleport-ent@13.0", "teleport-ent@11.0", "teleport-ent"]
+  conflicts_with formula: "teleport", cask: ["teleport", "teleport-ent@14.0", "teleport-ent@13.0", "teleport-ent"]
 
   caveats do
     license "https://dashboard.gravitational.com/web/"
